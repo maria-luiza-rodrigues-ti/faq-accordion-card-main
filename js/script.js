23a411a25cfa77369d48e041f2e21d72b375c67a
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2023-10-30 16:11:09 -0300

    feat: finishing icon animation

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
 const tabMenu = document.querySelectorAll(".accordion-list_item");
 const tabContent = document.querySelectorAll(".accordion-list_content");
+const tabTitle = document.querySelectorAll(".accordion-list_title");
+const tabIcon = document.querySelectorAll(".accordion-list_icon");
 
 function activeTab(index) {
   tabContent.forEach((content) => {
     content.classList.remove("active");
   });
+
+  tabTitle.forEach((content) => {
+    content.classList.remove("title-active");
+  });
+
+  tabIcon.forEach((content) => {
+    content.classList.remove("icon-active");
+  });
+
   tabContent[index].classList.add("active");
+  tabTitle[index].classList.add("title-active");
+  tabIcon[index].classList.add("icon-active");
 }
 
 tabMenu.forEach((itemMenu, index) => {
